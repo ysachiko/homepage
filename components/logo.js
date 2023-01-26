b06cc0802284f0b266f6ae5e76484ff91da9c66b
Kovalev Yurii <yurakovalev99@gmail.com>
2023-01-26 17:02:42 +0300

    initial commit

---
+++ components/logo.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+import {Text, useColorModeValue} from "@chakra-ui/react";
+import styled from "@emotion/styled"
+import FootprintIcon from './icons/footprint'
+
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-seize: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+
+  &:hover img {
+    transform: rotate(20deg);
+  }
+`
+
+const Logo = () => {
+    const footPrintImg = `/icons/footprint${useColorModeValue('', '-dark')}.png`;
+
+    return (
+        <Link href="/">
+                <LogoBox>
+                    {/* <Image src={footPrintImg} alt="logo" width={30} height={30} /> */}
+                    <FootprintIcon />
+                    <Text 
+                        color={useColorModeValue("gray.800", "whiteAlpha.900")} 
+                        fontFamily="M PLUS Rounded 1c" 
+                        fontWeight="bold" 
+                        ml={3}>Yura Kovalev
+                    </Text>
+                </LogoBox>
+        </Link>
+    )
+}
+
+export default Logo;
---
+++ pages/index.js
@@ -0,0 +1,131 @@
+import NextLink from 'next/link';
+import {
+    Link,
+    Container,
+    Heading,
+    Box,
+    SimpleGrid,
+    Button,
+    List,
+    ListItem,
+    Image,
+    useColorModeValue,
+    chakra
+  } from '@chakra-ui/react'
+import Section from "../components/section";
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioSection, BioYear } from '../components/bio';
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mt={10} mb={6} align="center">
+                Hi, I'm a frontend developer based in Moscow, Russia.
+            </Box>
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                 <Heading as="h2" variant="page-title">
+                    Yura Kovalev
+                 </Heading>
+                 <p>
+                    Internet Serfer ( Musician / Developer / Flower Boy)
+                 </p>
+                </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Box
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        w="100px"
+                        h="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        overflow="hidden"
+                    >
+                        <Image 
+                            borderColor="whiteAlpha.800" 
+                            borderWidth={2} 
+                            borderStyle="solid" 
+                            maxWidth="100px" 
+                            display="inline-block" 
+                            borderRadius="full" 
+                            src="/images/YK.png" 
+                            alt="profile image"
+                        /> 
+                    </Box>
+                </Box>
+            </Box>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                    <Paragraph>
+                    Greetings, friends. 
+                    As you may have already read, 
+                    I am a frontend developer, working for the Russian company "Samokat". 
+                    I made this site to flex and learn Next.JS and Chakra. 
+                    I'm in love with vinyl, make a smart home, and am a delicious cook. 
+                    You can always subscribe to me on {' '}
+                        <Link as={NextLink} href="https://www.instagram.com/fluttybeats/" passHref scroll={false}>
+                            instagram
+                        </Link>
+                    . Also you can check my github, there are no particularly interesting projects there yet, but someday there will be.
+                    </Paragraph>
+                    <Box align="center" my={4}>
+                        <Button
+                            as={NextLink}
+                            href="/works"
+                            scroll={false}
+                            rightIcon={<ChevronRightIcon />}
+                            colorScheme="teal"
+                        >
+                            my git
+                        </Button>
+                    </Box>
+            </Section>
+
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    Bio
+                </Heading>
+                <BioSection>
+                    <BioYear>
+                        1999
+                    </BioYear>
+                    Born in Omsk, Russia.
+                </BioSection>
+                <BioSection>
+                    <BioYear>
+                        2021
+                    </BioYear>
+                   Worked at <Link as={NextLink} href="https://fabuma.ru/" passHref scroll={false}>
+                            Fabuma Records.
+                        </Link>
+                </BioSection>
+                <BioSection>
+                    <BioYear>
+                        2022
+                    </BioYear>
+                    Became a computer security specialist Institute of Cybernetics MIREA.
+                </BioSection>
+                <BioSection>
+                    <BioYear>
+                        2023 to present
+                    </BioYear>
+                    Works at Samokat.
+                </BioSection>
+            </Section>
+        </Container>
+        
+    )
+}
+
+
+
+export default Page;
