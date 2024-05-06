0bafda5c8bddc50c11aba8d456aab06c631c65d7
Yura Kovalev <yurakovalev99@gmail.com>
2024-05-06 22:03:57 +0300

    add video

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { AspectRatio, Box,Heading } from "@chakra-ui/react"
 import VoxelRoom from '../components/voxel-room'
 import NoSsr from "../components/no-ssr";
 
@@ -8,6 +8,17 @@ const PostPage = () => {
             <NoSsr>
                 <VoxelRoom initialScale={3} modelName='/computer_terminal.glb' />
             </NoSsr>
+            <Heading>МОЛОДОЙ ЮРА - ПЕЩЕРНЫЙ КУНЖ</Heading>
+            <AspectRatio mt={4} maxW='760px' ratio={1}>
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/AspkYYHpp98?si=zJXEmsbFBkwUtdtj" 
+                    title="YouTube video player" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen></iframe>
+            </AspectRatio>
         </Box>
     )
 }
