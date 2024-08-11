c1bdb88b920ceb00b06cac57b77d781c7c31ca3a
Yura Kovalev <yurakovalev99@gmail.com>
2024-08-11 11:51:25 +0300

    add title

--- pages/vinyl.tsx
+++ pages/vinyl.tsx
@@ -1,4 +1,4 @@
-import {Box, Heading, SimpleGrid} from "@chakra-ui/react"
+import {Box, Heading, SimpleGrid, Text} from "@chakra-ui/react"
 import VoxelRoom from '../components/voxel-room'
 import NoSsr from "../components/no-ssr";
 import Image from "next/image";
@@ -17,6 +17,27 @@ const StyledTitle = styled(Heading)`
     }
 `
 
+const data = [
+    {src: "/images/vinyl/img.png", title: 'Frank Ocean - Blonde'},
+    {src: "/images/vinyl/img_1.png", title: 'Daft Punk - Random Access Memories'},
+    {src: "/images/vinyl/img_2.png", title: 'JPEGMAFIA - All My Heroes Are Cornballs'},
+    {src: "/images/vinyl/img_3.png", title: 'David Bowie - The Rise and Fall of Ziggy Stardust and the Spiders from Mars'},
+    {src: "/images/vinyl/img_4.png", title: 'Tyler, the Creator - Call Me If You Get Lost'},
+    {src: "/images/vinyl/img_5.png", title: 'Tyler, the Creator - Flower Boy'},
+    {src: "/images/vinyl/img_6.png", title: 'Kids See Ghosts'},
+    {src: "/images/vinyl/img_8.png", title: 'Frank Ocean - Nostalgia, Ultra'},
+    {src: "/images/vinyl/img_9.png", title: 'Kendrick Lamar - To Pimp a Butterfly'},
+    {src: "/images/vinyl/img_10.png", title: '100 gecs - 10,000 gecs'},
+    {src: "/images/vinyl/img_11.png", title: 'dog shit 💩'},
+    {src: "/images/vinyl/img_12.png", title: 'Portishead - Dummy'},
+    {src: "/images/vinyl/img_13.png", title: 'Madvillain - Madvillainy '},
+    {src: "/images/vinyl/img_14.png", title: 'FKA twigs - Caprisongs'},
+    {src: "/images/vinyl/img_15.png", title: 'Burial - Burial'},
+    {src: "/images/vinyl/img_16.png", title: 'The Beatles - Abbey Road'},
+    {src: "/images/vinyl/img_17.png", title: 'Siouxsie and the Banshees - Juju'},
+    {src: "/images/vinyl/img_18.png", title: 'Джунгли – Весна В Шанхае'},
+]
+
 const PostPage = () => {
     return(
         <Box>
@@ -25,60 +46,14 @@ const PostPage = () => {
             </NoSsr>
             <StyledTitle>My Vinyl</StyledTitle>
             <StyledGrid minChildWidth='300px' spacing='40px'>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img.png"} alt={'vinyl'} height={300} width={300}/>
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_1.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_2.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_3.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_4.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_5.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_6.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_8.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_9.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_10.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_11.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_12.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_13.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_14.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_15.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_16.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_17.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
-                <Box maxH='300px' maxW='300px'>
-                    <Image src={"/images/vinyl/img_18.png"} alt={'vinyl'} height={300} width={300}  />
-                </Box>
+                {
+                    data.map((record) =>
+                        <Box  maxW='300px' key={record.title}>
+                            <Image src={record.src} alt={'vinyl'} height={300} width={300}/>
+                            <Text fontSize='xl'>{record.title}</Text>
+                        </Box>)
+                }
+
             </StyledGrid>
         </Box>
     )
